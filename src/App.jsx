9a86573b6279d22ea07ed3d8b115f648a034cb3b
Noman <nomanmia128@gmail.com>
2024-10-18 19:11:39 +0600

    My First Coment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Books from './Books'
+import MyButton from './MyButton'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to my App</h1>
+      <Books></Books>
+      <MyButton></MyButton>
+    </>
+  )
+}
+
+export default App
---
+++ src/Books.jsx
@@ -0,0 +1,7 @@
+export default function Books(){
+    return (
+        <div>
+            <h3>BooksName: </h3>
+        </div>
+    )
+}
